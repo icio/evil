e8cea2f6a7ef3c5b8ab58bc8b605e620e2030725
Paul Scott <paul.scotto@gmail.com>
2014-02-21 21:44:21 +0000

    Initial setup

---
+++ setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(
+    name="setquery",
+    version="0.1",
+    description="Set arithmetic evaluator",
+    author="Paul Scott",
+    author_email="paul@duedil.com",
+    url="https://github.com/icio/setquery",
+
+    py_modules=["setquery"],
+    setup_requires=["nose"],
+    license="MIT",
+)
