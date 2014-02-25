470ca128a39ba2f9ccc432195b28b787bd3581f6
Paul Scott <paul.scotto@gmail.com>
2014-02-25 19:18:09 +0000

    0.2 Tag

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from setuptools import setup
 
 setup(
     name="setquery",
-    version="0.1",
+    version="0.2",
     description="Set arithmetic evaluator",
     author="Paul Scott",
     author_email="paul@duedil.com",
     url="https://github.com/icio/setquery",
-    download_url="https://github.com/icio/setquery/tarball/0.1",
+    download_url="https://github.com/icio/setquery/tarball/0.2",
     setup_requires=["nose", "rednose"],
     py_modules=["setquery", "test_setquery"],
     license="MIT",
