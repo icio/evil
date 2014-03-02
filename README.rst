972167a1f2dd1c3ec369e739b01efef0d2ea79c7
Paul Scott <paul.scotto@gmail.com>
2014-03-02 14:06:40 +0000

    Remove test setup from setup.py

---
+++ README.rst
@@ -0,0 +1,16 @@
+Setquery
+========
+
+Setquery is a utility library for evaluating single expressions composed of patterns and operators across sets, allowing user expressivity beyond what command-line arguments can express.
+
+Development
+-----------
+
+:code: bash
+   # Prepare environment:
+   virtualenv .
+   . bin/activate
+   pip install nose rednose
+
+   # Run tests
+   python setup.py -q nosetests
--- setup.py
+++ setup.py
@@ -3,14 +3,13 @@ from setuptools import setup
 
 setup(
     name="setquery",
-    version="0.2",
+    version="0.3",
     description="Set arithmetic evaluator",
     author="Paul Scott",
     author_email="paul@duedil.com",
     url="https://github.com/icio/setquery",
-    download_url="https://github.com/icio/setquery/tarball/0.2",
-    setup_requires=["nose", "rednose"],
-    py_modules=["setquery", "test_setquery"],
+    download_url="https://github.com/icio/setquery/tarball/0.3",
+    py_modules=["setquery"],
     license="MIT",
     keywords=['set', 'expression', 'eval', 'evaluate'],
     classifiers=[],
